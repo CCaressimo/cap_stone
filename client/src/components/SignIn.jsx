b70e879e29ff8c0896a00cd6d91b80af8f7465e6
Frank Gomez <frank9g7@gmail.com>
2021-07-14 18:45:03 -0500

    work on the snackbar table

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -17,8 +17,9 @@ export default function SignIn() {
               </div>
               <hr class="mt-6 border-b-1 border-blueGray-300"/>
       <form>
-          <div class="relative w-full mb-3">
+          <div class="relative w-full mb-3 item-center">
             <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-password">Email</label>
+          <CgUser />
             <input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Email"></input>
           </div>
 
--- client/src/components/Snackbar.jsx
+++ client/src/components/Snackbar.jsx
@@ -1,29 +1,106 @@
 import React from "react";
+import "./css/snackbar.css"
+import burger from './Images/burger.jpg'
+import { IoFastFoodOutline } from "react-icons/io5";
+
 export default function Snackbar() {
   return (
-    <main>
-      <section className="relative pt-36 lg:pt-48 px-20">
-        <div className="splashSize pt-24 md:pt-48 items-center">
-          <div className="splashBx border border-gray-400 rounded-t rounded-b px-4">
-            <h1 className="splashText my-4 text-3xl md:text-5xl text-center">
-              Snackbar
-            </h1>
-            <h2 className="text-xs md:text-2xl text-center">
-              snack/food/drink ideas that fit genre selection
-            </h2>
-            <nav>
-              <div className="text-center py-6">
-                <button
-                  to="/about"
-                  className="nextButton text-2xl py-2 px-4 border border-gray-200 rounded shadow"
-                >
-                  Find a Restaurant
-                </button>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </section>
-    </main>
+   <main>
+     <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+    <table className = "min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+          <tr >
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">image</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">item</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+          </tr>
+      </thead>
+      <tbody class="bg-white divide-y divide-gray-200">
+          <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                <div  class="flex-shrink-0 h-20 w-20" >
+                <img src={burger} alt="burger" class="h-20 w-20 rounded-full" />
+                </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-lg text-gray-900 ">
+                  burgers
+                </div>
+              
+              </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                <div>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">TAKE</a>
+                </div>
+                <div>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">DROP</a>
+                </div>
+                </td>
+
+         </tr>
+         <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                <div  class="flex-shrink-0 h-20 w-20" >
+                <img src={burger} alt="burger" class="h-20 w-20 rounded-full" />
+                </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-lg text-gray-900 ">
+                  burgers
+                </div>
+              
+              </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                <div>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">TAKE</a>
+                </div>
+                <div>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">DROP</a>
+                </div>
+                </td>
+
+         </tr>
+         <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                <div  class="flex-shrink-0 h-20 w-20" >
+                <img src={burger} alt="burger" class="h-20 w-20 rounded-full" />
+                </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-lg text-gray-900 ">
+                  burgers
+                </div>
+              
+              </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                <div>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">TAKE</a>
+                </div>
+                <div>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">DROP</a>
+                </div>
+                </td>
+
+         </tr>
+      </tbody>
+    </table>
+   
+      </div>
+      </div>
+      </div>
+      </div>
+      <div>
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">  Food Generator</button>
+      </div>
+      </main>
   );
 }
