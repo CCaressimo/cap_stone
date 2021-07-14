37091e55355a238c9c2ebeb7694b7bf6566fff56
Frank Gomez <frank9g7@gmail.com>
2021-07-13 20:44:59 -0500

    added the new sign in page

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -3,62 +3,30 @@ import "./css/Background.css"
 
 export default function SignIn() {
     return (
-      <main className="leading-normal tracking-normal">
-        <section className="relative leading-normal tracking-normal pt-6">
-          <div className="signinSize relative border border-gray-400 rounded-b rounded-t overflow-hidden shadow-lg leading-normal tracking-normal max-w-6xl mx-auto">
-            <form onSubmit="*" className="splashBx mx-auto text-center">
-              <div className="w-full m-0 p-12 bg-cover">
-                <div className="max-w-4xl mx-auto text-center break-normal">
-                  <p className="headerText shadow-inner font-bold text-3xl md:text-5xl">
-                    Sign In
-                  </p>
-                </div>
-              </div>
-              <div className="">
-                <div className="">
-                  <label
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    for="grid-password"
-                  ></label>
-                  <input
-                    className="appearance-none block w-full bg-white text-gray-700 border border-gray-800 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                  />
-                </div>
-                <div className="w-full md:w-1/2 pr-6 pl-3">
-                  <label
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    for="grid-last-name"
-                  ></label>
-                  <input
-                    className="appearance-none block w-full bg-white text-gray-700 border border-gray-800 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-last-name"
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                  />
-                </div>
-              </div>
-              <div className="pb-6">
-                <a href="/signup" className="signUpStyle">
-                  Sign up
-                </a>
-              </div>
-              <div className="text-center pb-6">
-                <button
-                  className="headerText nextButton text-2xl py-2 px-4 border border-gray-200 rounded shadow"
-                  type="submit"
-                  value="Send"
-                >
-                  Log in
-                </button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
+       <div class="container mx-auto p-4 h-full relative pt-36 lg:pt-48 px-20
+       ">
+      <div class="flex content-center items-center justify-center h-full">
+      <div class="w-full lg:w-4/12 px-4">
+        <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-200 border-0">
+          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+      <form>
+        <div class="relative w-full mb-3">
+        <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="grid-password">Email</label>
+        <input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Email"></input>
+        </div>
+
+        <div class="relative w-full mb-3">
+        <label class="block uppercase text-gray-600 text-xs font-bold mb-2" for="grid-password">Password</label>
+        <input type="password" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Password">
+        </input>
+        </div>
+        <div class="text-center mt-6"><button class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none" type="button">Sign In</button>
+        </div>
+      </form>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
     );
 }
