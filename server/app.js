a3a3c7a0969d5bd4f0f700ef2c5485349f52ca45
amerlinoutnabout <73296588+amerlinoutnabout@users.noreply.github.com>
2021-07-15 09:10:01 -0700

    backend start

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const app = express();
+const db = require('./queries');
+const { snack, sequelize } = require("./models");
+const es6Renderer = require("express-es6-template-engine");
+const bodyParser = require('body-parser')
+
+
+
+app.use(helmet());
+app.use(express.json());
+app.use(cors())
+
+if (process.env.NODE_ENV === 'development') {
+app.use(morgan('tiny'));
+}
+
+app.get('/', function (req, res) {
+res.send('Express application working ...');
+});
+
+app.get('/fares', db.getFares)
+
+//app.get('/fares/:genre', db.getFaresByGenre)
+
+app.get("/fares/:genre", db.getFaresByGenre)
+
+
+// Start server
+app.listen(8080, () => console.log('server running ...'))
+
+module.exports = app;
